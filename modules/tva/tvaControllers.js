b5b00934c64d0ba442a9154d41872aa61acf7fae
yann <yann.boisselier@gmail.com>
2015-11-26 02:10:17 +0100

    add exercices

--- modules/tva/tvaControllers.js
+++ modules/tva/tvaControllers.js
@@ -1,10 +1,16 @@
 angular.module('tva')
 
 // injection de dépendance explicite
-.controller('tvaController', ['$scope', 'calculTVA', function($scope, calculTVA) {
+.controller('tvaController', ['$scope', 'calculTVA', '$http', function($scope, calculTVA, $http) {
 
   $scope.montant = 0;
   $scope.tva = 20;
+
+  $scope.tvaIsZero = function() {
+    if (0 === parseInt($scope.tva)) {
+      return true;
+    }
+  };
 
   // instanciation de la "classe" calculMontant
 
@@ -18,5 +24,26 @@ angular.module('tva')
   $scope.montantRandom = function() {
     $scope.montant = Math.random() * 10000;
   };
-  
+
+  // fake Items
+  $scope.items = [{
+    title: "titre 1",
+  }, {
+    title: "titre 2",
+  }, {
+    title: "titre 3"
+  }];
+
+  // json Items
+  $http({
+    method: 'GET',
+    url: 'http://jsonplaceholder.typicode.com/posts'
+  }).then(function successCallback(response) {
+    $scope.posts = response.data;
+  }, function errorCallback(response) {
+    alert("http request failed");
+  });
+
+
+
 }]);
